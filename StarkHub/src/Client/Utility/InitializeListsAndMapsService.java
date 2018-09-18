f4d2c0b8af423d84a384d705ab491d697ea05de2
Abhishek1103 <kumar.abh494@gmail.com>
2018-09-18 14:22:39 +0530

    Completed initialiser(De-Serialisation of objets)

--- StarkHub/src/Client/Utility/InitializeListsAndMapsService.java
+++ StarkHub/src/Client/Utility/InitializeListsAndMapsService.java
@@ -1,7 +1,15 @@
 package Client.Utility;
 
+import Client.DataClasses.Channel;
+import Client.Login.Main;
+import Client.UI.MainPageController;
+import hubFramework.Video;
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class InitializeListsAndMapsService extends Service {
     @Override
@@ -13,7 +21,43 @@ public class InitializeListsAndMapsService extends Service {
                 try{
 
                     // TODO: Add functionality
-                    System.out.println("\n\n Add Functionality to me \n\n");
+                    //System.out.println("\n\n Add Functionality to me \n\n");
+
+                    File f = new File(System.getProperty("user.home") + "/starkhub/"+ "watchlater/list");
+                    if(!f.exists()){
+                        MainPageController.watchLaterList = new ArrayList<>();
+                    }
+                    ObjectInputStream ois = new ObjectInputStream(new FileInputStream(f));
+                    MainPageController.watchLaterList = (ArrayList<Video>) ois.readObject();
+                    ois.close();
+                    System.out.println("Read WatchLater");
+
+                    f = new File(System.getProperty("user.home") + "/starkhub/"+ "history/list");
+                    if(!f.exists()){
+                        MainPageController.historyList = new ArrayList<>();
+                    }
+                    ois = new ObjectInputStream(new FileInputStream(f));
+                    MainPageController.historyList = (ArrayList<Video>) ois.readObject();
+                    ois.close();
+                    System.out.println("Read History");
+
+                    f = new File(System.getProperty("user.home") + "/starkhub/"+ "mychannels/list");
+                    if(!f.exists()){
+                        MainPageController.myChannelMap= new HashMap<>();
+                    }
+                    ois = new ObjectInputStream(new FileInputStream(f));
+                    MainPageController.myChannelMap = (HashMap<String, Channel>) ois.readObject();
+                    ois.close();
+                    System.out.println("Read Mychannels");
+
+                    f = new File(System.getProperty("user.home") + "/starkhub/"+ "subscriptions/list");
+                    if(!f.exists()){
+                        MainPageController.subscribedChannelMap= new HashMap<>();
+                    }
+                    ois = new ObjectInputStream(new FileInputStream(f));
+                    MainPageController.subscribedChannelMap = (HashMap<String, String>) ois.readObject();
+                    ois.close();
+                    System.out.println("Read Subscriptions");
 
                 }catch (Exception e){
                     e.printStackTrace();
@@ -22,4 +66,6 @@ public class InitializeListsAndMapsService extends Service {
             }
         };
     }
+
+
 }
